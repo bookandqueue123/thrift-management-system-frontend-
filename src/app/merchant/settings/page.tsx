9202fbf32e6a1680f5a588fb4d8f97777b3d45f7
olaidris0 <olasunkanmi.idris@raoatech.com>
2024-02-21 20:38:38 +0100

    currently on settings Screen

--- src/app/merchant/settings/page.tsx
+++ src/app/merchant/settings/page.tsx
@@ -1,9 +1,38 @@
+import { CustomButton } from '@/components/Buttons';
+import Image from 'next/image';
 import React from 'react'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
-  )
+    <>
+      <div className="mb-4 space-y-2">
+        <p className="text-base font-bold text-ajo_offWhite text-opacity-60">
+          Savings Settings
+        </p>
+        {/* <p className="text-sm text-ajo_offWhite">
+          Savings settings
+        </p> */}
+      </div>
+      <div>
+        <Image
+          src="/receive-money.svg"
+          alt="hand with coins in it"
+          width={120}
+          height={120}
+        />
+        <p className="text-ajo_offWhite">
+          Create a savings group make all the necessary edits and changes. Use
+          the button below to get started!
+        </p>
+        <CustomButton
+          type="button"
+          label="Savings SetUp"
+          style="rounded-md bg-ajo_blue py-3 px-9 text-sm text-ajo_offWhite  hover:bg-indigo-500 focus:bg-indigo-500 md:w-[60%]"
+          // onButtonClick={onSubmitHandler}
+        />
+      </div>
+    </>
+  );
 }
 
 export default Settings
