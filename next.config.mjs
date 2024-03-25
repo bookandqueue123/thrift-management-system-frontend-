492eb84fd7930160a3178cd92717bef17b5340c4
olaidris0 <olasunkanmi.idris@raoatech.com>
2024-03-25 11:59:49 +0100

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-// const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "production";
 
-const apiEnv = process.env.NEXT_PUBLIC_API_ENV || "development";
-
+console.log("production", isProd);
+console.log("isProdValue", process.env.NODE_ENV)
 
 const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
   env: {
-    BACKEND_API:
-      apiEnv === "production"
-        ? "https://prod-api.finkia.com.ng/"
-        : "https://staging-api.finkia.com.ng/",
+    BACKEND_API: isProd
+      ? "https://prod-api.finkia.com.ng/"
+      : "https://staging-api.finkia.com.ng/",
   },
 };
 
