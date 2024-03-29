1e6478da2a854cfddd7edeca9b31cbfaed24841b
olaidris0 <olasunkanmi.idris@raoatech.com>
2024-03-29 14:48:49 +0100

    added an home button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="flex h-screen items-center justify-center bg-ajo_offWhite">
@@ -8,6 +10,11 @@ export default function NotFound() {
         <p className="mt-4 text-ajo_darkBlue">
           The page you are looking for does not exist.
         </p>
+        <Link href="/">
+          <a className="ripple mt-6 inline-block rounded-md bg-ajo_blue px-6 py-2 text-center text-sm font-semibold leading-6 text-white shadow transition hover:shadow-lg focus:outline-none">
+            Go to Home
+          </a>
+        </Link>
       </div>
     </div>
   );
