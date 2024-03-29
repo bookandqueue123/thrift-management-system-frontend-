d9dde35cbe2bcc7e93143dcd74d74793ffd63bfe
ajy14 <emmanuel.ajibade@raoatech.com>
2024-03-29 12:45:23 +0100

    transactions filter

--- src/app/merchant/page.tsx
+++ src/app/merchant/page.tsx
@@ -13,12 +13,17 @@ import { useAuth } from "@/api/hooks/useAuth";
 import { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { allSavingsResponse, customer, savings } from "@/types";
-import { useState } from "react";
+import { ChangeEvent, SetStateAction, useState } from "react";
 import { extractDate, extractTime, formatToDateAndTime } from "@/utils/TimeStampFormatter";
 import { Badge } from "@/components/StatusBadge";
+import { CiExport } from "react-icons/ci";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md"
 const MerchantDashboard = () => {
   const PAGE_SIZE = 2;
   const { client } = useAuth();
+  const [fromDate, setFromDate] = useState('');
+  const [toDate, setToDate] = useState('');
   const [currentPage, setCurrentPage] = useState(1)
   const [filteredTransactions, setFilteredTransactions] = useState<savings[]>([])
 const organizationId = useSelector(selectOrganizationId)
@@ -46,7 +51,38 @@ const { data: allTransactions, isLoading: isLoadingallTransactions } = useQuery(
   },
 });
 
+const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+  // setSearchResult(e.target.value);
+  console.log(e.target.value)
 
+  console.log(allTransactions.savings)
+  if(allTransactions){
+    const filtered = allTransactions.savings.filter((item: {
+      [x: string]: any; accountNumber: any; 
+}) =>
+      String(item.user.accountNumber).includes(String(e.target.value))
+  );
+  // Update the filtered data state
+  setFilteredTransactions(filtered);
+  }
+  
+};
+
+const handleDateFilter = () => {
+  // Filter the data based on the date range
+  if(allTransactions){
+  const filtered = allTransactions.savings.filter((item: { createdAt: string | number | Date; }) => {
+    const itemDate = new Date(item.createdAt); // Convert item date to Date object
+    const startDateObj = new Date(fromDate);
+    const endDateObj = new Date(toDate);
+
+    return itemDate >= startDateObj && itemDate <= endDateObj;
+  });
+
+  // Update the filtered data state
+  setFilteredTransactions(filtered);
+}
+};
 
 console.log(filteredTransactions)
 const paginatedTransactions = filteredTransactions.slice(
@@ -67,7 +103,7 @@ const paginatedTransactions = filteredTransactions.slice(
 
 let totalPages = 0
 if(allTransactions){
-   totalPages = Math.ceil(allTransactions.length / PAGE_SIZE);
+   totalPages = Math.ceil(allTransactions.savings.length / PAGE_SIZE);
 }
 
 const goToPreviousPage = () => {
@@ -80,6 +116,16 @@ const goToNextPage = () => {
   if (currentPage < totalPages) {
    (setCurrentPage(currentPage + 1));
   }
+};
+const handleFromDateChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+  setFromDate(event.target.value);
+  
+};
+
+const handleToDateChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+  setToDate(event.target.value);
+  
+  handleDateFilter()
 };
 
 
@@ -152,32 +198,32 @@ console.log(paginatedTransactions)
           </p>
           <span className="flex items-center gap-3">
             {/* <SearchInput onSearch={() => ("")}/> */}
-            {/* <form className="flex items-center justify-between rounded-lg bg-[rgba(255,255,255,0.1)] p-3">
-      <input
-      onChange={(e) => console.log(12)}
-        type="search"
-        placeholder="Search"
-        className="w-full bg-transparent text-ajo_offWhite caret-ajo_offWhite outline-none focus:outline-none"
-      />
-      <svg width="16" height="16" viewBox="0 0 18 18" fill="none">
-        <circle
-          cx="8.60996"
-          cy="8.10312"
-          r="7.10312"
-          stroke="#EAEAFF"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M13.4121 13.4121L16.9997 16.9997"
-          stroke="#EAEAFF"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </form> */}
+            <form className="flex items-center justify-between rounded-lg bg-[rgba(255,255,255,0.1)] p-3">
+              <input
+              onChange={handleSearch}
+                type="search"
+                placeholder="Search"
+                className="w-full bg-transparent text-ajo_offWhite caret-ajo_offWhite outline-none focus:outline-none"
+              />
+              <svg width="16" height="16" viewBox="0 0 18 18" fill="none">
+                <circle
+                  cx="8.60996"
+                  cy="8.10312"
+                  r="7.10312"
+                  stroke="#EAEAFF"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M13.4121 13.4121L16.9997 16.9997"
+                  stroke="#EAEAFF"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </form>
             <FilterDropdown
               options={[
                 "Timestamp",
@@ -192,6 +238,32 @@ console.log(paginatedTransactions)
           </span>
         </div>
 
+        <div className="flex items-center">
+                <p className="mr-2 font-lg text-white">Select range from:</p>
+                <input
+                  type="date"
+                  value={fromDate}
+                   onChange={handleFromDateChange}
+                  className="px-4 py-2 w-48 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                />
+
+
+                <p className="mx-2 text-white">to</p>
+                <input
+                  type="date"
+                  value={toDate}
+                  onChange={handleToDateChange}
+                  className="px-4 py-2 w-48 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                />
+              </div>
+              <div className="flex mt-4">
+                <button className="mr-4 bg-transparent hover:bg-blue-500 text-white font-medium hover:text-white py-2 px-4 border border-white hover:border-transparent rounded flex">Export as CSV <span className="ml-2 mt-1"><CiExport /></span></button>
+                <button className="px-4 py-2 text-white rounded-md border-none bg-transparent relative">
+                  
+                  <u>Export as Excel</u>
+                </button>
+              </div>
+
         <div>
           <p className="pl-2 text-xs text-ajo_offWhite">
             *Please Scroll sideways to view all content
@@ -200,6 +272,11 @@ console.log(paginatedTransactions)
             headers={["TransactionDate", 
                       "Reference", 
                       "Customer Name", 
+                      "Account Number",
+                      "Purpose",
+                      "Time",
+                      "Payment Mode",
+                      
                       "Email Address", 
                       "Phone Number",
                       "Channel",
@@ -215,6 +292,19 @@ console.log(paginatedTransactions)
                 <td className="whitespace-nowrap px-6 py-4 text-sm">
                   {transaction.user.firstName} {transaction.user.lastName}
                 </td>
+                <td className="whitespace-nowrap px-6 py-4 text-sm">
+                  {transaction.user.accountNumber}
+                </td>
+                <td className="whitespace-nowrap px-6 py-4 text-sm">
+                  {transaction.purposeName}
+                </td>
+                <td className="whitespace-nowrap px-6 py-4 text-sm">
+                  {extractTime(transaction.updatedAt)}
+                </td>
+                <td className="whitespace-nowrap px-6 py-4 text-sm">
+                 Payment Mode
+                </td>
+
                 <td className="whitespace-nowrap px-6 py-4 text-sm">
                   {transaction.user.email}
                 </td>
@@ -238,7 +328,52 @@ console.log(paginatedTransactions)
               </tr>
             ))}
           />
-          <PaginationBar apiResponse={DummyTransactions} />
+
+<div className="flex justify-center items-center  space-x-2">
+            <button
+              className="p-2 border border-blue-500 rounded-md hover:bg-blue-100 focus:outline-none focus:ring focus:border-blue-300"
+              onClick={goToPreviousPage}
+            >
+              <MdKeyboardArrowLeft />
+            </button>
+
+            <button
+              className="p-2  text-blue-500 rounded-md cursor-pointer hover:bg-blue-100 focus:outline-none focus:ring focus:border-blue-300"
+              onClick={() => setCurrentPage(currentPage)}
+            >
+              {currentPage}
+            </button>
+
+            <button
+              className="p-2  rounded-md cursor-pointer hover:bg-blue-100 focus:outline-none focus:ring focus:border-blue-300"
+              onClick={() =>(setCurrentPage(currentPage + 1))}
+            >
+              {currentPage + 1}
+            </button>
+            <button
+              className="p-2  rounded-md cursor-pointer hover:bg-blue-100 focus:outline-none focus:ring focus:border-blue-300"
+              onClick={() =>(setCurrentPage(currentPage + 2))}
+            >
+              {currentPage + 2}
+            </button>
+
+            <button
+              className="p-2 border border-blue-500 rounded-md hover:bg-blue-100 focus:outline-none focus:ring focus:border-blue-300"
+              onClick={goToNextPage}
+            >
+              <MdKeyboardArrowRight />
+            </button> 
+
+              {/* <button
+                className="p-2 bg-white rounded-md cursor-pointer hover:bg-blue-100 focus:outline-none focus:ring focus:border-blue-300"
+                onClick={() => dispatch(setCurrentPage(currentPage + 6))}
+              >
+                {currentPage + 6}
+              </button> */}
+
+                    
+         </div>
+          {/* <PaginationBar apiResponse={DummyTransactions} /> */}
         </div>
       </section>
     </>
