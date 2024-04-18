73eaf6457615ec4f9fb71417b944f71d9a3b3888
ajy14 <emmanuel.ajibade@raoatech.com>
2024-04-18 18:51:10 +0100

    fixes

--- src/modules/HomePage/NavBar.tsx
+++ src/modules/HomePage/NavBar.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
+
 export default function Navbar(){
     return(
         <nav className="">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-[#EAAB40]">Finkia</span>
+                    <Image
+                    src="/Logo.svg"
+                    width={100}
+                    height={100}
+                    alt="logo"
+                    />
+                    {/* <span className="self-center text-2xl font-semibold whitespace-nowrap text-[#EAAB40]">Finkia</span> */}
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
