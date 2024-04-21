afde6846f1e797ea351e877597d56db0facd0a48
olaidris0 <olasunkanmi.idris@raoatech.com>
2024-04-21 02:24:05 +0100

    fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import HomePage from "@/modules/HomePage/homePage";
-import { tailspin } from "ldrs";
+// import { tailspin } from "ldrs";
+import type {} from "ldrs";
 import { useEffect, useState } from "react";
 import Landing from "./landing";
 
@@ -9,12 +10,17 @@ export default function Home() {
   const [isHomepageSet, setIsHomepageSet] = useState(false);
   const [host, setHost] = useState("");
 
-  tailspin.register();
   useEffect(() => {
+    async function getLoader() {
+      const { tailspin } = await import("ldrs");
+      tailspin.register();
+    }
+    getLoader();
+
     if (typeof window !== "undefined") {
       const url = new URL(window.location.href);
       setHost(url.host);
-      console.log("host", host);
+      // console.log("host", host);
     }
 
     if (host === "finkia.com.ng" || host === "staging.finkia.com.ng") {
@@ -27,12 +33,11 @@ export default function Home() {
 
   if (!isHomepageSet) {
     return (
-      // <div className="al flex bg-ajo_darkBlue">
-      <div className="flex h-[100vh] items-center justify-center bg-ajo_darkBlue">
+      <div className="flex h-[100vh] justify-center bg-ajo_darkBlue pt-[10%]">
         <l-tailspin
-          size="100"
+          size="80"
           stroke="8"
-          speed="0.9"
+          speed="0.8"
           color="#F2F0FF"
         ></l-tailspin>
       </div>
