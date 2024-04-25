5300c3ef0b6de84f5e607d8b44f679da1809bb17
olaidris0 <olasunkanmi.idris@raoatech.com>
2024-04-25 21:52:32 +0100

    fixes

--- src/modules/HomePage/organisation/OrganisationHomepage.tsx
+++ src/modules/HomePage/organisation/OrganisationHomepage.tsx
@@ -6,6 +6,7 @@ import ProductService from "../ProuctService";
 import FAQ from "../Faq";
 import ContactUs from "../ContactUs";
 import Footer from "../Footer";
+import { useState, useEffect } from "react";
 
 
 const data = [
@@ -18,20 +19,38 @@ const data = [
     
   ];
 const aboutText = "Maxwell savings is a traditional thrift management company - popularly known as ajo, esusu or adashe. The company was founded 1st February, 2017. As a customer, you can choose to save your money with Maxwell Savings on a daily and weekly basis. And you can also collect your saved funds on a monthly, quarterly or yearly basis."
-export default function OrganisationHomePage(){
-    return(
-        <div >
-           <Navbar/>
-            <HeroSection 
-             bg="white"
-            /> 
-            <AboutUs bg="white" data={data} aboutText={aboutText}/>
-            {/* <HowToUse/> */}
-            {/* <ProductService/>
+export default function OrganisationHomePage() {
+  const [host, setHost] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const url = new URL(window.location.href);
+      setHost(url.host);
+      console.log("host", host);
+    }
+  }, [host]);
+    return (
+      <div>
+        <Navbar />
+        <HeroSection bg="white" />
+        <AboutUs bg="white" data={data} aboutText={aboutText} />
+        {/* <HowToUse/> */}
+        {/* <ProductService/>
             <FAQ/> */}
-            <ContactUs bg="white"/>
-            <Footer/>
-        </div>
-        
-    )
+        <ContactUs
+          bg="white"
+          contactPhone={
+            host === "www.maxwellsaving.com.ng"
+              ? "+234 906 190 3588"
+              : "+234 809 722 7051"
+          }
+          contactMail={
+            host === "www.maxwellsaving.com.ng"
+              ? "info@maxwellsavings.com.ng"
+              : "finkia.support@raoatech.com"
+          }
+        />
+        <Footer />
+      </div>
+    );
 }
