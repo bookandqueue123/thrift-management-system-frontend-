0b5d16caec876fdba968ee99fa98fce1006c4ce7
ajy14 <emmanuel.ajibade@raoatech.com>
2024-06-06 17:49:28 +0100

    url

--- src/api/hooks/useAuth.tsx
+++ src/api/hooks/useAuth.tsx
@@ -3,11 +3,11 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 
-//export const apiUrl = process.env.BACKEND_API;
-export const apiUrl = 'http://localhost:4000/'
+export const apiUrl = process.env.BACKEND_API;
+//export const apiUrl = 'http://localhost:4000/'
 // console.log(apiUrl);
 
- const BASE_URL = apiUrl;
+const BASE_URL = apiUrl;
 
 export const useAuth = () => {
   const token = useSelector(selectToken);
