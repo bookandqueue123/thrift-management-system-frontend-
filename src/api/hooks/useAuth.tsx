15f38d0600cf27e5ff5cce12a03d9172ccc90624
ajy14 <emmanuel.ajibade@raoatech.com>
2024-08-01 11:08:12 +0100

    changes

--- src/api/hooks/useAuth.tsx
+++ src/api/hooks/useAuth.tsx
@@ -3,10 +3,10 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 
-// export const apiUrl = process.env.BACKEND_API;
-export const apiUrl = 'http://localhost:4000/'
+export const apiUrl = process.env.BACKEND_API;
+//export const apiUrl = 'http://localhost:4000/'
 
-//export const apiUrl = 'https://raotech-backend.onrender.com'
+
 // console.log(apiUrl);
 
 const BASE_URL = apiUrl;
