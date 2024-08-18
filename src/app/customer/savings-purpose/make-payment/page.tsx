41d97b836528768b20e1c45903ec68cfd69b3dc6
ajy14 <emmanuel.ajibade@raoatech.com>
2024-08-18 07:29:08 +0100

    payment push

--- src/app/customer/savings-purpose/make-payment/page.tsx
+++ src/app/customer/savings-purpose/make-payment/page.tsx
@@ -186,7 +186,7 @@ export default function MakePayment() {
             <div className="flex items-center justify-center min-h-screen ">
                 <div className="text-center">
                 <h1 className="text-4xl font-bold mb-8 text-white">Make Payment</h1>
-                {allGateways.map((gateway: { _id: Key | null | undefined; fixedFee: number; percentageFee: number; }) => (
+                {allGateways?.map((gateway: { _id: Key | null | undefined; fixedFee: number; percentageFee: number; }) => (
                     <button 
                     key={gateway._id}
                     onClick={() => makeThePayment(gateway.fixedFee, gateway.percentageFee)}
