25fbd78c09d1ce78c18aa5fbbb4d99b9d2820e01
ajy14 <emmanuel.ajibade@raoatech.com>
2024-09-29 11:34:57 +0100

    amount formatter fix

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useAuth } from "@/api/hooks/useAuth";
+import { apiUrl, useAuth } from "@/api/hooks/useAuth";
 import { CustomButton } from "@/components/Buttons";
 import Modal from "@/components/Modal";
 import SuccessToaster, { ErrorToaster } from "@/components/toast";
@@ -10,6 +10,7 @@ import {
 } from "@/slices/OrganizationIdSlice";
 import AmountFormatter from "@/utils/AmountFormatter";
 import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { useRouter } from "next/navigation";
 
@@ -178,28 +179,28 @@ export default function Pricing() {
           amount = servicePackage.actualFee.actualYearlyFee;
         }
 
-        // const response = await axios.post(
-        //   `${apiUrl}api/pay/flw/subscription-payment`,
+        const response = await axios.post(
+          `${apiUrl}api/pay/flw/subscription-payment`,
 
-        //   {
-        //     referralName,
-        //     redirectURL,
-        //     environment,
-        //     paymentFor,
-        //     userId,
-        //     packageId,
-        //     paymentPlan,
-        //     amount,
-        //     phoneNumber,
-        //     email,
-        //     customerName,
-        //   },
-        //   { headers: { Authorization: `Bearer ${token}` } },
-        // );
+          {
+            referralName,
+            redirectURL,
+            environment,
+            paymentFor,
+            userId,
+            packageId,
+            paymentPlan,
+            amount,
+            phoneNumber,
+            email,
+            customerName,
+          },
+          { headers: { Authorization: `Bearer ${token}` } },
+        );
 
-        // if (response.data.status === "success") {
-        //   window.location.href = response.data.data.link;
-        // }
+        if (response.data.status === "success") {
+          window.location.href = response.data.data.link;
+        }
       } catch (error) {
         console.error(error);
       }
