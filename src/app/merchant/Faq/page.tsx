5f3045bd3946394cacab618ce63cf35fa7925312
Favourmbata <134517996+Favourmbata@users.noreply.github.com>
2025-07-08 18:21:49 +0100

    Navbar

---
+++ src/app/merchant/Faq/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FAQ from '@/modules/HomePage/Faq'
+const page = () => {
+  return (
+    <div>
+      <FAQ/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/modules/merchant/Ratings-Review/Ratings-Review.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RatingsReview = () => {
+  return (
+    <div className='text-ajo_offWhite'>
+      page
+    </div>
+  )
+}
+
+export default RatingsReview;
